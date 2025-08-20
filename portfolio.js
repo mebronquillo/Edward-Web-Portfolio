4a2cb7e3dac29d32de4ad5c3f9274c1d89559b82
Edward? <108749515+mebronquillo@users.noreply.github.com>
2025-08-20 18:40:45 +0800

    first commit

---
+++ portfolio.js
@@ -0,0 +1,34 @@
+
+function direct_to_project(){
+    const scrolltoproject = document.getElementById("im_the_project");
+    scrolltoproject.scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+var body = document.body;
+function direct_to_aboutme(){
+    document.getElementById("nav_aboutme_id").style.width = "100%";
+    body.classList.toggle("dont_scroll");
+    
+}
+
+function close_nav(){
+    document.getElementById("nav_aboutme_id").style.width = "0%";
+}
+
+
+function direct_to_contactme(){
+    document.getElementById("nav_contact_id").style.width = "500px";
+}
+
+function close_nav1(){
+    document.getElementById("nav_contact_id").style.width = "0%";
+}
+
+
+
+
+
+
+
