dd1932f5d5041f25efa0d61ab168df18aca15b98
Edward Ronquillo <mebronquillo@tip.edu.ph>
2025-11-03 19:22:51 +0800

    updated

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,14 @@
 
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction(){
+    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
+        document.getElementById("nav_id").style.padding = "15px 10px";
+    } else{
+        document.getElementById("nav_id").style.padding = "40px 10px";
+    }
+}
 function direct_to_project(){
     const scrolltoproject = document.getElementById("im_the_project");
     scrolltoproject.scrollIntoView({
@@ -7,6 +17,8 @@ function direct_to_project(){
         inline: "start"
     });
 }
+
+
 
 function direct_to_contactme(){
     const scrolltocontactme = document.getElementById("im_the_contactme");
@@ -17,21 +29,30 @@ function direct_to_contactme(){
     });
 }
 
+
+
+
 var body = document.body;
+
 function direct_to_aboutme(){
     document.getElementById("nav_aboutme_id").style.width = "100%";
     body.classList.toggle("dont_scroll");
     
 }
 
+
+
 function close_nav(){
     document.getElementById("nav_aboutme_id").style.width = "0%";
+    
 }
 
-function toggleMenu(){
-    document.getElementById("nav_Menu").classList.toggle("nav_active");
-}
+document.getElementById("aos_anims").addEventListener("click", function(){
+    const anim_aoss = document.getElementById("show_aos");
+    anim_aoss.style.display = "block";
 
+    AOS.refresh();
+});
 
 
 
@@ -56,7 +77,7 @@ const randomTexts = [
     "Seize the day!",
     "Dream Big!",
     "What do you call a farm working on a medicine? A 'Farmmacist!'",
-    
+    "One day I'll buy my favorite Road Bike!"
 ];
 
 hoverArea.addEventListener('mouseenter', () => {
@@ -79,6 +100,8 @@ randomTextDisplay.style.display = 'none';
 });
 });
 
+
+// aos animation
 
 
 
